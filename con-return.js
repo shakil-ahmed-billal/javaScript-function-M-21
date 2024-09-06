d093d536ffad85d72e66a5ef1c03bf9c3ad0b5e8
shakil-ahmed-billal <itzxhakil@gmail.com>
2024-09-06 22:23:51 +0600

    function recap js

---
+++ con-return.js
@@ -0,0 +1,17 @@
+function isEven (number){
+    if (number % 2 === 0){
+        return true ;
+    }
+    else{
+        return false;
+    }
+}
+console.log(isEven(5));
+console.log(isEven(106));
+
+function isOdd (number){
+    if (number % 2 === 1){
+        return true ;
+    }
+    return false ;
+}
--- recap.js
+++ recap.js
@@ -10,3 +10,15 @@ function add2(price1 , price2){
 const bill2 = add(5,80)
 
 console.log(bill, bill2);
+
+function doMath(num1 , num2){
+    const sum = num1 + num2 ;
+    const deff = num1 - num2 ;
+    const multiply = sum * deff ;
+    const result = multiply / 2 ;
+    return result;
+}
+
+const result = doMath(10,5)
+console.log(result);
+
