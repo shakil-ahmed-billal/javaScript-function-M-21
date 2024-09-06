21aae4cbe228c4cd006e85fefca16acbb993b88b
shakil-ahmed-billal <itzxhakil@gmail.com>
2024-09-06 16:21:22 +0600

    paramiter part complete

---
+++ function.js
@@ -0,0 +1,15 @@
+const number = 45 ;
+function fanOffkor() {
+    console.log('bosha theke othe dara');
+    console.log('walk towards the swich');
+    console.log('click the switch to off');
+}
+
+function brashTeeth(){
+    console.log('pick up the brash');
+    console.log('add paste');
+    console.log('2 min gose gose barsh koro');
+}
+
+//function call
+fanOffkor();
---
+++ parameters.js
@@ -0,0 +1,39 @@
+function vatkhaw(){
+    console.log('hand wash kore new');
+    console.log('bose bose khaw');
+    console.log('plete khabar dabar new');
+    console.log('gopat gopat kore khaw');
+}
+
+vatkhaw();
+
+
+function square(number){
+    console.log(number);
+    console.log('value of the number parameters', number);
+    const borgo = number * number ;
+    console.log('square of the number number is :', borgo);
+}
+
+square(4);
+console.log("-------------");
+square(8);
+console.log('-------------');
+square(14);
+
+console.log('---------------');
+function add (num1, num2){
+    const sum = num1 + num2 ;
+    console.log('number of the sum',sum);
+}
+
+add(5,7)
+
+console.log("------------");
+function adALL (a,b,c,d,e,f){
+    const total = a+b+c+d+e+f;
+    console.log(a,b,c,d,e,f);
+    console.log(total);
+}
+
+adALL(3,4,5,6,7,7)
